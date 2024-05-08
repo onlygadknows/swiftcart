864dfbe9fca098a1dc303a85c4f87e88cbcbc460
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-05-08 20:51:54 +0800

    Update: Adding Footer and Header for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>Welcome to SwiftCart</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to SwiftCart</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
