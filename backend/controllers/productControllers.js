c0bf2162139d7f60ed543d4d475df6d56c6341e0
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-05-13 17:31:01 +0800

    Update: Finished updating productController.js

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,11 +1,17 @@
 import asyncHandler from "../middleware/asyncHandler.js"
 import Product from "../models/productModel.js";
 
+//@desc Fetch all product
+//@route GET /api/products
+//@access Public
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products)
 });
 
+//@desc Fetch all product
+//@route GET /api/products/:id
+//@access Public
 const getProductsById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
     if(product) {
