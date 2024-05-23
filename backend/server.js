c22bd3b24e5e59e1116f25f2b3bd5b3ebb50e0ca
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-05-24 06:57:03 +0800

    Trying to fix CORS

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,10 @@ import { ALLOWED_ORIGIN } from './utils/allowedOrigin.js';
 app.use(cors({
     origin: ALLOWED_ORIGIN, 
     credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Request-With'],
   }));
 
-    
 connectDB(); // connect to MongoDB
 
 //body parser middleware
