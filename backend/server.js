fbea4f5027977928a287d6ef3bb9e80e3f89d6ba
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-05-24 03:19:28 +0800

    Fixing minor UI errors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 const app = express()
 
 app.use(cors({
@@ -29,22 +29,31 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 
-
-app.get('/', (req, res) => {
-    res.send('API is running')
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
 
-const __dir = path.resolve();
-app.use('/uploads', express.static(path.join(__dir, '/uploads'))); // Correctly set up static file serving
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); // Correctly set up static file serving
+app.use('/product_images', express.static(path.join(__dirname, 'dist', 'product_images')));
 
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.use(notFound)
-app.use(errorHandler)
+    //any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+ );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    });
+    
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
