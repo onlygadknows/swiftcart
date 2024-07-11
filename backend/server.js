1d0e871f9b35df984efc7b646b01dcd6b4f86969
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-11 20:28:36 +0800

    Rollback

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express()
 // import { ALLOWED_ORIGIN } from './utils/allowedOrigin.js';
 
 app.use(cors({
-    origin: 'https://swiftcart-fw2u.onrender.com/', 
+    origin: '*', 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
