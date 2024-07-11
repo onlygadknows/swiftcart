4c7108473df506ef3c1548c80744e41ec29a35b1
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-12 00:11:00 +0800

    zxc

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ import { ALLOWED_ORIGIN } from './utils/allowedOrigin.js';
 const corsOptions = {
     origin: ALLOWED_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   };
   app.use(cors(corsOptions));
 
