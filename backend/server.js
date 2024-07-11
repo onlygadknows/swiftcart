6d0faaf12d6f27ef1ba2e9db6eae724ec27e0212
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-11 21:40:43 +0800

    will try

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,11 @@ const app = express()
 //   }));
 
 const corsOptions = {
-    origin: 'https://swiftcart-fw2u.onrender.com',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
   };
-  
   app.use(cors(corsOptions));
 
     
