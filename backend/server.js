6cc1a650f8546de0d0ee2bc8001e34d45a7914f3
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-12 21:29:38 +0800

    Will try

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,13 @@ const corsOptions = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
-  };
-  app.use(cors(corsOptions));
 
-    
+  };
+app.use(cors(corsOptions));
+app.options('*', cors());
 connectDB(); // connect to MongoDB
 
 //body parser middleware
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
