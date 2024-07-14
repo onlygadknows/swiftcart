fdafa376e622c6fd5024f424f40e3b80e4222c64
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-14 22:21:38 +0800

    Reroll

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api',
+        target: 'http://localhost:8000',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
