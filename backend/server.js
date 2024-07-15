db967e8ea587f813ba3843c72a639d1b3d9371ce
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-15 18:21:14 +0800

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -53,8 +53,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
     //any route that is not api will be redirected to index.html
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
- );
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 } else {
     app.get('/', (req, res) => {
         res.send('API is running')
