2c36e94b8c1d34ae43fd40649bc2e8a3a2c46d55
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-15 19:41:45 +0800

    zz

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost8000" : "";
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "http://localhost:8000";
 export const BASE = "http://localhost:8000";
 export const PRODUCTS_URL = "/api/products";
 export const PRODUCTS_URL_ID = "/api/products/:id";
