75c0b7928a339256116a9e739fcf775e5580f9e0
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-16 20:36:14 +0800

    Change logo in title

--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,6 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost8000" : "";
 export const BASE_URL = "https://swift-cart.onrender.com";
+// export const BASE_URL = "http://localhost4000";
 export const BASE = "https://swift-cart.onrender.com";
 export const PRODUCTS_URL = "/api/products";
 export const PRODUCTS_URL_ID = "/api/products/:id";
