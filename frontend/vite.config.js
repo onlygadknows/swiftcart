22bac74af7a80771aa6b2d1a4ea9c6d060b68589
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-16 19:54:23 +0800

    revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://swift-cart.onrender.com',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
     },
