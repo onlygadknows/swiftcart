c9e5f64ca72f199640e6b8b5d17fc30909da5962
Gad Ashell Sususco <gadashellsususco7@gmail.com>
2024-07-16 18:59:02 +0800

    Edit api target in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/',
+        target: 'https://swift-cart.onrender.com/',
         changeOrigin: true,
       },
     },
